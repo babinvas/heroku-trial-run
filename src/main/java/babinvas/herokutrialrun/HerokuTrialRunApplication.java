fb717f7a4e874eb9de50c99f8215d7283c8e190c
babinvas <babinvas@gmail.com>
2021-01-05 16:27:25 +0300

    Initial commit

---
+++ src/main/java/babinvas/herokutrialrun/HerokuTrialRunApplication.java
@@ -0,0 +1,13 @@
+package babinvas.herokutrialrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuTrialRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuTrialRunApplication.class, args);
+	}
+
+}
