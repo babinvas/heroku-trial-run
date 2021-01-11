94ac7ecaf33f71e3b99d0cbf6a793f4cd5cf3322
babinvas <babinvas@gmail.com>
2021-01-11 20:48:50 +0300

    RestController was implemented

--- src/main/java/babinvas/herokutrialrun/controllers/MainRestController.java
+++ src/main/java/babinvas/herokutrialrun/controllers/MainRestController.java
@@ -1,5 +1,7 @@
 package babinvas.herokutrialrun.controllers;
 
+import babinvas.herokutrialrun.services.SendRequestsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,23 @@ import javax.websocket.server.PathParam;
 
 @RestController
 public class MainRestController {
+	private final SendRequestsService sendRequestsService;
+
+	@Autowired
+	public MainRestController(SendRequestsService sendRequestsService) {
+		this.sendRequestsService = sendRequestsService;
+	}
+
 	@PostMapping("/rest/{id}")
 	public String receiveDeveloper(@PathParam("id") String id) {
-		return null;
+		long longId;
+
+		try {
+			longId = Long.getLong(id);
+		} catch (RuntimeException e) {
+			return "Необходимо вести id, как целое число.";
+		}
+
+		return sendRequestsService.receiveDeveloper(longId);
 	}
 }
