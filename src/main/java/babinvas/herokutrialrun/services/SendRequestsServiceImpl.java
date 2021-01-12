de704d431862250939cdd8f0de27179070299233
babinvas <babinvas@gmail.com>
2021-01-12 20:01:48 +0300

    The Service file have been corrected

--- src/main/java/babinvas/herokutrialrun/services/SendRequestsServiceImpl.java
+++ src/main/java/babinvas/herokutrialrun/services/SendRequestsServiceImpl.java
@@ -7,9 +7,10 @@ import org.springframework.stereotype.Service;
 public class SendRequestsServiceImpl implements SendRequestsService {
 	public String receiveDeveloper(long id) {
 		if (!Developer.hasId(id)) {
-			return "{\"Ошибка ввода\"}: {\"У нас такого разработчика нет! Извиняй!\"}";
+			return "Ошибка ввода: У нас такого разработчика нет! \n\n" +
+					"Извиняйте!";
 		}
 
-		return "{\"Наш разработчик:\"}: {\"" + Developer.getName(id) + "\"}";
+		return "Наш разработчик: " + Developer.getName(id);
 	}
 }
