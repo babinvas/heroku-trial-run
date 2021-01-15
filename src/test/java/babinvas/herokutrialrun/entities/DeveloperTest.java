b33c4b96c22c882177ca9c29c2d94cd1114822bf
babinvas <babinvas@gmail.com>
2021-01-15 19:04:08 +0300

    Empty test methods for Developer class have been created

---
+++ src/test/java/babinvas/herokutrialrun/entities/DeveloperTest.java
@@ -0,0 +1,16 @@
+package babinvas.herokutrialrun.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeveloperTest {
+
+	@Test
+	void hasId() {
+	}
+
+	@Test
+	void getName() {
+	}
+}
