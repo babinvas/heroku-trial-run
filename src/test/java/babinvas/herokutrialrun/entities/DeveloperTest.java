c805787fdf32b1bcb3ac17cda297d816e84cfbfe
babinvas <babinvas@gmail.com>
2021-01-15 19:22:43 +0300

    Two test methods were implemented

--- src/test/java/babinvas/herokutrialrun/entities/DeveloperTest.java
+++ src/test/java/babinvas/herokutrialrun/entities/DeveloperTest.java
@@ -1,16 +1,27 @@
 package babinvas.herokutrialrun.entities;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class DeveloperTest {
 
 	@Test
-	void hasId() {
+	void testHasId_ShouldReturnTrue() {
+		for (long i = 1L; i < 6L; i++) {
+			Assertions.assertTrue(Developer.hasId(i));
+		}
 	}
 
 	@Test
-	void getName() {
+	void testHasId_ShouldReturnFalse() {
+		long[] id = {-1L, 0L, 6L, 10L, 1000L};
+
+		for (long i : id) {
+			Assertions.assertFalse(Developer.hasId(i));
+		}
+	}
+
+	@Test
+	void testGetName() {
 	}
 }
